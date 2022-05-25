762eb9b99dee022e3ad6f773c83ae765ca8dadbb
Dwayne <dwayneokoye98@gmail.com>
2022-05-25 13:19:35 +0100

    before merge

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,6 +1,7 @@
 package com.qa.ims.persistence.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -18,21 +19,26 @@ public class ItemDAO {
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
-	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+	
+	//model
+	public Item modelitems(ResultSet resultSet) throws SQLException {
 		Long id = resultSet.getLong("id");
 		double cost = resultSet.getDouble("cost");
 		String name = resultSet.getString("name");
 		return new Item(id,cost,name);
 	}
 	
-	public List<Item> readAll() {
+	public List<Item> readallitem() {
 		try (Connection connection = DBUtils.getInstance().getConnection();
 				Statement statement = connection.createStatement();
 				ResultSet resultSet = statement.executeQuery("SELECT * FROM item");) {
+			
+			//items data type into array list
 			List<Item> item = new ArrayList<>();
 			while (resultSet.next()) {
-				item.add(modelFromResultSet(resultSet));
+				item.add(modelitems(resultSet));
 			}
+			//return of the array list
 			return item;
 		} catch (SQLException e) {
 			LOGGER.debug(e);
@@ -46,7 +52,7 @@ public class ItemDAO {
 				Statement statement = connection.createStatement();
 				ResultSet resultSet = statement.executeQuery("SELECT * FROM item ORDER BY id DESC LIMIT 1");) {
 			resultSet.next();
-			return modelFromResultSet(resultSet);
+			return modelitems(resultSet);
 		} catch (Exception e) {
 			LOGGER.debug(e);
 			LOGGER.error(e.getMessage());
@@ -54,4 +60,16 @@ public class ItemDAO {
 		return null;
 	
 }
+	
+	public int deleteItem(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM item WHERE id = ?");) {
+			statement.setLong(1, id);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
 }
