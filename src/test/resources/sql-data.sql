75d22a6f3cbfc4667222ab6f12c6a9c3c9b3e6ee
Dwayne <dwayneokoye98@gmail.com>
2022-05-26 01:27:49 +0100

    edit customer test

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `customers` (`first_name`, `surname`,username,password) VALUES ('jordan', 'harrison',"username","password");
